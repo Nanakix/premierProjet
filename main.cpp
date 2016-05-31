84b45a96aeb4136153378cb395d99bc429b5602c
bsauzet <bsauzet@outlook.fr>
2016-05-31 08:41:28 +0200

    ajout fenetre parent

--- main.cpp
+++ main.cpp
@@ -9,14 +9,21 @@ int main(int argc, char *argv[])
     QWidget fenetre;
     fenetre.setFixedSize(300,150);
 
-    QPushButton bouton("Salut les Zéros, la forme ?");
+    // création d'un bouton
+    QPushButton bouton("Salut les Zéros, la forme ?", &fenetre);
+
+    // personnalisation du bouton
     bouton.setText("HELLO MA FRRIEND ! cyka blyat");
     bouton.setToolTip("idi nahui blyat");
+
     QFont mahFont("Comic Sans MS");
+
     bouton.setFont(mahFont);
     bouton.setCursor(Qt::PointingHandCursor);
     bouton.setIcon(QIcon("C:/Users/Jean/Documents/QtProjects/premierProjet/HLBM.png"));
-    bouton.show();
+
+    // Affichage
+    fenetre.show();
 
     return app.exec();
 }
